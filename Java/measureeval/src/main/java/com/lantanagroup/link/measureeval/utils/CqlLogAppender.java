8472f71a64b75ca4fb20d1d8f88a1e051d517587
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2024-12-16 12:13:51 -0800

    Grouping the output resources from logging by resource to keep the logs shorter.

--- Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/utils/CqlLogAppender.java
+++ Java/measureeval/src/main/java/com/lantanagroup/link/measureeval/utils/CqlLogAppender.java
@@ -7,6 +7,8 @@ import com.lantanagroup.link.measureeval.services.MeasureEvaluatorCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,8 +26,11 @@ public class CqlLogAppender extends AppenderBase<ILoggingEvent> {
             String range = matcher.group(2);
             String output = matcher.group(3)
                     .replaceAll("org.hl7.fhir.r4.model.", "")
-                    .replaceAll("@[0-9A-Fa-f]{8}", "");
+                    .replaceAll("@[0-9A-Fa-f]{6,8}", "");
             String cql = null;
+
+            // Group the resources in the output
+            output = groupResources(output);
 
             MeasureEvaluatorCache measureEvalCache = ApplicationContextProvider.getApplicationContext().getBean(MeasureEvaluatorCache.class);
             MeasureEvaluator evaluator = measureEvalCache.get(libraryId);       // Assume the measure id is the same as the library id since the log entry doesn't output the measure url/id
@@ -37,6 +42,38 @@ public class CqlLogAppender extends AppenderBase<ILoggingEvent> {
             // Custom processing with libraryId and range
             processLogEntry(libraryId, range, output, cql);
         }
+    }
+
+    private String groupResources(String output) {
+        if (output == null || output.isEmpty() || output.equals("{}") || !output.startsWith("{")) {
+            return output;
+        }
+
+        output = output.substring(1, output.length() - 1);
+
+        if (output.endsWith(",")) {
+            output = output.substring(0, output.length() - 1);
+        }
+
+        String[] resources = output.split(",");
+        Map<String, Integer> resourceCount = new HashMap<>();
+
+        for (String resource : resources) {
+            resourceCount.put(resource, resourceCount.getOrDefault(resource, 0) + 1);
+        }
+
+        StringBuilder groupedOutput = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : resourceCount.entrySet()) {
+            if (groupedOutput.length() > 0) {
+                groupedOutput.append(",");
+            }
+            groupedOutput.append(entry.getKey());
+            if (entry.getValue() > 1) {
+                groupedOutput.append("(").append(entry.getValue()).append(")");
+            }
+        }
+
+        return groupedOutput.toString();
     }
 
     private void processLogEntry(String libraryId, String range, String output, String cql) {
