6e47401a23e78f73d7f2f7e94ed58343412ed3d6
Steven Williams <steven.williams@lantanagroup.com>
2025-03-18 06:20:11 -0400

    Add constants for diagnostic names

---
+++ Java/shared/src/main/java/com/lantanagroup/link/shared/utils/DiagnosticNames.java
@@ -0,0 +1,13 @@
+package com.lantanagroup.link.shared.utils;
+
+public class DiagnosticNames {
+    public static final String CORRELATION_ID = "correlation.id";
+    public static final String FACILITY_ID = "facility.id";
+    public static final String FREQUENCY = "frequency";
+    public static final String PATIENT_ID = "patient.id";
+    public static final String PERIOD_END = "period.end";
+    public static final String PERIOD_START = "period.start";
+    public static final String QUERY_TYPE = "query.type";
+    public static final String REPORT_ID = "report.id";
+    public static final String REPORT_TYPE = "report.types";
+}
