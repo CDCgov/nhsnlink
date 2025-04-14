9ef90e4cdfd4b52936d6def0ffacfa1616babe2f
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2025-04-14 14:02:34 -0700

    Adding unit tests to Shared project for HtmlSanitizer

---
+++ DotNet/SharedTests/Application/Services/Security/HtmlInputSanitizerTest.cs
@@ -0,0 +1,24 @@
+using LantanaGroup.Link.Shared.Application.Services.Security;
+using Xunit;
+
+namespace LantanaGroup.Link.SharedTests.Application.Services.Security;
+
+public class HtmlInputSanitizerTest
+{
+    [Theory]
+    [InlineData("<script>alert('XSS');</script>", "")]
+    [InlineData("alert('XSS');</script>", "alertXSS")]
+    [InlineData("Hello, World!", "Hello World")]
+    [InlineData("123-456_789", "123-456_789")]
+    [InlineData("!@#$%^&*()", "amp")]       // Sanitizer class converts & to &amp; then regex removes & and ;
+    [InlineData("", "")]
+    [InlineData(null, "")]
+    public void TestSanitizeAndRemove(string input, string expected)
+    {
+        // Act
+        var result = HtmlInputSanitizer.SanitizeAndRemove(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
