60525d727c0644741e13edf09793acca48bc9000
Keith Kissal <99497673+kissalk@users.noreply.github.com>
2025-04-25 10:24:14 -0400

    updating ps1

--- Scripts/git_commit.ps1
+++ Scripts/git_commit.ps1
@@ -1,13 +1,19 @@
 # Path to your JSON file
-$jsonPath = "$(Build.SourcesDirectory)/DotNet/Account/appsettings.json"
+$jsonPath = "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json"
+
+# Service Name
+$serviceName = $env:SERVICENAME
 
 # Load the JSON file
 $json = Get-Content $jsonPath -Raw | ConvertFrom-Json
 
+# Gathers the current GitCommit value
 $old = $json.ServiceInformation.GitCommit
 
-$new = "12344"
+# Updates the GitCommit value
+$new = "$env:GIT_COMMIT"
 
-(Get-Content -Path "$(Build.SourcesDirectory)/DotNet/Account/appsettings.json") | ForEach-Object { $_ -replace $old, $new } | Set-Content -Path "$(Build.SourcesDirectory)/DotNet/Account/appsettings.json"
+(Get-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json") | ForEach-Object { $_ -replace $old, $new } | Set-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json"
 
-Write-Host $json
+# ECHOs the Appsettings.json value
+Get-Content -Path "$env:BUILD_SOURCESDIRECTORY/DotNet/$env:SERVICENAME/appsettings.json"
